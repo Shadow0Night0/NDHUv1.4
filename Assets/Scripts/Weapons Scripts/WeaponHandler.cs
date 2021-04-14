81976df2a67413e6108d39eed2a064b54349f73e
LABPC-GROUP <410721330@gms.ndhu.edu.tw>
2021-04-14 23:08:43 +0800

    Added Weapon Animations ( no script yet )

---
+++ Assets/Scripts/Weapons Scripts/WeaponHandler.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
