ab0f49c33898e94957dcf80b9352c0ca92041cc8
LABPC-GROUP <410721330@gms.ndhu.edu.tw>
2021-04-28 22:58:42 +0800

    M for map

--- Assets/Scripts/Canvas Scripts/MinimapScript.cs
+++ Assets/Scripts/Canvas Scripts/MinimapScript.cs
@@ -7,6 +7,29 @@ public class MinimapScript : MonoBehaviour
 
     public Transform player;
 
+    [SerializeField] GameObject ndhuMap;
+
+    // Minimap script in canvas because
+    // we cant activate a script form initially disabled GameObject
+
+    public static bool isMapOpen = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (isMapOpen)
+            {
+                CloseMap();
+            }
+            else
+            {
+                OpenMap();
+            }
+        }
+            
+    }
+
     private void LateUpdate()
     {
         // moving minimap camera with player
@@ -18,4 +41,19 @@ public class MinimapScript : MonoBehaviour
         transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
 
     }
+    
+    // Open map
+
+    void OpenMap()
+    {
+        ndhuMap.SetActive(true);
+        isMapOpen = true;
+    }
+    void CloseMap()
+    {
+        ndhuMap.SetActive(false);
+        isMapOpen = false;
+
+    }
+
 }
